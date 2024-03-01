497802836f0b42f3ae0bfa50d2ee60446790d75b
Crryptid <tomogatchi0.01@gmail.com>
2024-03-01 22:20:28 +0700

    add main hello go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Crryptid/app-say-hello
+
+go 1.21.7
+
+require github.com/Crryptid/go-say-hello v1.0.0 // indirect
---
+++ gohelloapp.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	gosayuser "github.com/Crryptid/go-say-hello"
+)
+
+func main() {
+	fmt.Println(gosayuser.HiUser("Ahmad"))
+}
